1edfd4b3a147f3dc40f19ef5828d25b8eaeed8bc
Sahaj Jain <jainsahaj@gmail.com>
2024-03-09 05:12:29 +0000

    done

--- index.js
+++ index.js
@@ -1,48 +1,90 @@
 import { Client, GatewayIntentBits, WebhookClient } from "discord.js";
 
-// Replace with your actual environment variables
 const DISCORD_TOKEN = Bun.env.DISCORD_TOKEN;
-const SOURCE_CHANNEL_ID = Bun.env.SOURCE_CHANNEL_ID;
-const WEBHOOK_ID = Bun.env.WEBHOOK_ID;
-const WEBHOOK_TOKEN = Bun.env.WEBHOOK_TOKEN;
+const CHANNEL_IDS = Bun.env.CHANNEL_IDS.split(",");
+const CHANNEL_LANGS = Bun.env.CHANNEL_LANGS.split(",");
+const WEBHOOK_IDS = Bun.env.WEBHOOK_IDS.split(",");
+const WEBHOOK_TOKENS = Bun.env.WEBHOOK_TOKENS.split(",");
 
-const client = new Client({
-  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent],
+const channels = CHANNEL_IDS.map((e, i) => {
+  return {
+    id: e,
+    lang: CHANNEL_LANGS[i],
+    webhook: new WebhookClient({
+      id: WEBHOOK_IDS[i],
+      token: WEBHOOK_TOKENS[i],
+    }),
+  };
 });
 
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
 
-client.on('ready', async () => {
+client.on("ready", async () => {
   console.log(`Logged in as ${client.user?.tag}!`);
 });
 
-client.on('messageCreate', async (message) => {
-  if (!message.guild || message.channel.id !== SOURCE_CHANNEL_ID) return;
-
-  const webhook = new WebhookClient({ id: WEBHOOK_ID, token: WEBHOOK_TOKEN });
+client.on("messageCreate", async (message) => {
+  var idx = CHANNEL_IDS.indexOf(message.channel.id);
+  if (!message.guild || idx < 0 || message.author.bot || message.webhookID)
+    return;
 
   try {
-    const translationResponse = await Bun.fetch("https://translate.sahajjain.com/translate", {
-      method: "POST",
-      body: JSON.stringify({
-        q: message.content,
-        source: "en",
-        target: "de",
-        format: "text"
-      }),
-      headers: { "Content-Type": "application/json" },
-    });
+    await Promise.all(
+      channels.map(async (e, i) => {
+        if (i !== idx) {
+          const translationResponse = await Bun.fetch(
+            "https://translate.sahajjain.com/translate",
+            {
+              method: "POST",
+              body: JSON.stringify({
+                q: message.content,
+                source: channels[idx].lang,
+                target: e.lang,
+                format: "text",
+              }),
+              headers: { "Content-Type": "application/json" },
+            }
+          );
 
-    const translationData = await translationResponse.json();
-    console.log(message)
-    const translatedText = translationData.translatedText;
+          const translationData = await translationResponse.json();
+          const translatedText = translationData.translatedText;
 
-    await webhook.send({
-      content: translatedText,
-      username: message.author.globalName,
-      avatarURL: message.author.avatarURL() || message.author.defaultAvatarURL,
-    });
+          await e.webhook.send({
+            content: translatedText,
+            username: message.author.globalName,
+            avatarURL:
+              message.author.avatarURL() || message.author.defaultAvatarURL,
+          });
 
-    console.log(`Translated "${message.content}" to "${translatedText}" and sent to target channel.`);
+          console.log(
+            `Translated "${message.content}" to "${translatedText}" and sent to target channel.`
+          );
+
+          if(message.attachments.size > 0) {
+            const targetChannel = message.guild.channels.cache.get(e.id);
+            for (const attachment of message.attachments.values()) {
+              const attachmentUrl = attachment.url;
+              const fileName = attachment.name;
+              try {
+                await targetChannel.send({
+                  content: `**${message.author.globalName}:**`,
+                  files: [{ attachment: attachmentUrl, name: fileName }],
+                });
+                console.log(`Successfully forwarded attachment: ${fileName}`);
+              } catch (error) {
+                console.error(`Error forwarding attachment: ${error}`);
+              }
+            }
+          }
+        }
+      })
+    );
   } catch (error) {
     console.error("Error during translation or webhook sending:", error);
   }
