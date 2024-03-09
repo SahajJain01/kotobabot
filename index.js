482b013a7da451f6955d7dcc80237603a46f8519
Sahaj Jain <jainsahaj@gmail.com>
2024-03-09 10:03:10 +0000

    nickname

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ client.on("messageCreate", async (message) => {
 
           await e.webhook.send({
             content: content,
-            username: message.author.displayName,
+            username: message.member.nickname,
             avatarURL:
               message.author.avatarURL() || message.author.defaultAvatarURL,
           });
