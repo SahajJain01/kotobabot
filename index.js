d6ddf1dc8f8653f8f88405df7b746ecc92217bf8
Sahaj Jain <jainsahaj@gmail.com>
2024-03-16 11:06:40 +0000

    added-teimstamp

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-import { Client, GatewayIntentBits } from "discord.js";
+import { Client, GatewayIntentBits, SlashCommandBuilder } from "discord.js";
+import { DateTime } from "luxon";
 
 const TRANSLATE_APIURL = Bun.env.TRANSLATE_APIURL;
 const DISCORD_TOKEN = Bun.env.DISCORD_TOKEN;
@@ -6,17 +7,116 @@ const DISCORD_TOKEN = Bun.env.DISCORD_TOKEN;
 let channels;
 let channelIds;
 
+const errStr =
+  "Invalid time format. Please provide the time in the format HH:MM.";
+
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildIntegrations,
     GatewayIntentBits.MessageContent,
   ],
 });
 
+const extractAndRemoveUrls = function (text) {
+  const urlRegex = /https?:\/\/[^\s]+/g; // Match URLs starting with http or https
+  const matches = text.matchAll(urlRegex);
+  const extractedUrls = [];
+
+  let newText = text;
+  for (const match of matches) {
+    extractedUrls.push(match[0]);
+    newText = newText.replace(match[0], "");
+  }
+
+  return {
+    extractedUrls,
+    text: newText,
+  };
+};
+
+const getChannelNames = async function () {
+  const chs = client.guilds.cache.first()?.channels.cache.values();
+
+  if (!chs) {
+    console.log("No channels found.");
+    return;
+  }
+
+  channels = [];
+  channelIds = [];
+
+  for (const channel of chs) {
+    const category = channel.parent;
+    if (category && category.name.toLowerCase().includes("translate")) {
+      const webhooks = await channel.fetchWebhooks();
+      let webhook;
+      if (webhooks.size) {
+        webhook = webhooks.first();
+      } else {
+        webhook = await channel.createWebhook({
+          name: "Webhook",
+        });
+        console.log("Webhook created for " + channel.name);
+      }
+      channels.push({
+        lang: channel.name.slice(-2),
+        webhook: webhook,
+      });
+      channelIds.push(channel.id);
+    }
+  }
+  console.log("Built channel list");
+};
+
+const getResStr = function (utcTimestamp) {
+  return `<t:${utcTimestamp}:t> ->   \\<t:${utcTimestamp}:t>\n<t:${utcTimestamp}:R> ->   \\<t:${utcTimestamp}:R>`;
+};
+
 client.on("ready", async () => {
   console.log(`Logged in as ${client.user?.tag}!`);
   getChannelNames();
+
+  const atCommand = new SlashCommandBuilder()
+    .setName("at")
+    .setDescription(
+      "Converts a given time in a specific timezone to a UTC Discord timestamp."
+    )
+    .addStringOption((option) =>
+      option
+        .setName("time")
+        .setDescription("The time in the format HH:MM")
+        .setRequired(true)
+    )
+    .addStringOption((option) =>
+      option
+        .setName("timezone")
+        .setDescription("Your local timezone.")
+        .setRequired(true)
+        .addChoices(
+          { name: "PST - Pacific Standard Time (UTC-8)", value: "UTC-8" },
+          { name: "MST - Mountain Standard Time (UTC-7)", value: "UTC-7" },
+          { name: "CST - Central Standard Time (UTC-6)", value: "UTC-6" },
+          { name: "EST - Eastern Standard Time (UTC-5)", value: "UTC-5" },
+          { name: "CET - Central European Time (UTC+1)", value: "UTC+1" },
+          { name: "EET - Eastern European Time (UTC+2)", value: "UTC+2" },
+          { name: "WET - Western European Time (UTC+0)", value: "UTC+0" },
+          { name: "IST - India Standard Time (UTC+5:30)", value: "UTC+5:30" }
+        )
+    );
+  client.application.commands.create(atCommand);
+
+  const inCommand = new SlashCommandBuilder()
+    .setName("in")
+    .setDescription("Converts given hours/minutes to a UTC Discord timestamp.")
+    .addStringOption((option) =>
+      option.setName("hours").setDescription("How many hours from now.")
+    )
+    .addStringOption((option) =>
+      option.setName("minutes").setDescription("How many minutes from now.")
+    );
+  client.application.commands.create(inCommand);
 });
 
 client.on("channelUpdate", (oldChannel, newChannel) => {
@@ -113,57 +213,54 @@ client.on("messageCreate", async (message) => {
   }
 });
 
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isCommand()) return;
+
+  const { commandName, options } = interaction;
+
+  if (commandName === "at") {
+    const timeString = options.get("time").value;
+    const timezoneString = options.get("timezone").value;
+    try {
+      const dateTime = DateTime.fromFormat(timeString, "HH:mm", {
+        zone: timezoneString,
+      });
+      const utcTimestamp = dateTime.setZone("UTC").toUnixInteger();
+
+      await interaction.reply({
+        content: getResStr(utcTimestamp),
+        ephemeral: true,
+      });
+    } catch (error) {
+      await interaction.reply({
+        content: errStr + error,
+        ephemeral: true,
+      });
+    }
+  }
+
+  if (commandName === "in") {
+    const hours = options.get("hours")?.value;
+    const minutes = options.get("minutes")?.value;
+    try {
+      const currentDateTime = DateTime.utc();
+      const utcTimestamp = currentDateTime
+        .plus({ hours: hours, minutes: minutes })
+        .toUnixInteger();
+
+      await interaction.reply({
+        content: getResStr(utcTimestamp),
+        ephemeral: true,
+      });
+    } catch (error) {
+      await interaction.reply({
+        content: errStr + error,
+        ephemeral: true,
+      });
+    }
+  }
+});
+
 client.login(DISCORD_TOKEN).catch((error) => {
   console.error("Login error:", error);
 });
-
-function extractAndRemoveUrls(text) {
-  const urlRegex = /https?:\/\/[^\s]+/g; // Match URLs starting with http or https
-  const matches = text.matchAll(urlRegex);
-  const extractedUrls = [];
-
-  let newText = text;
-  for (const match of matches) {
-    extractedUrls.push(match[0]);
-    newText = newText.replace(match[0], "");
-  }
-
-  return {
-    extractedUrls,
-    text: newText,
-  };
-}
-
-async function getChannelNames() {
-  const chs = client.guilds.cache.first()?.channels.cache.values();
-
-  if (!chs) {
-    console.log("No channels found.");
-    return;
-  }
-
-  channels = [];
-  channelIds = [];
-
-  for (const channel of chs) {
-    const category = channel.parent;
-    if (category && category.name.toLowerCase().includes("translate")) {
-      const webhooks = await channel.fetchWebhooks();
-      let webhook;
-      if (webhooks.size) {
-        webhook = webhooks.first();
-      } else {
-        webhook = await channel.createWebhook({
-          name: "Webhook",
-        });
-        console.log("Webhook created for " + channel.name);
-      }
-      channels.push({
-        lang: channel.name.slice(-2),
-        webhook: webhook,
-      });
-      channelIds.push(channel.id);
-    }
-  }
-  console.log("Built channel list");
-}
